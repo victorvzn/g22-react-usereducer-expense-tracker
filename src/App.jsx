cf4ad8a62b14c3a4501515bfa1de88e05d93fd5b
Victor Villazon <victorzn23@gmail.com>
2023-09-12 19:50:36 -0500

    maquetación del expense tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 function App() {
   return (
     <>
-      <h1 className='text-4xl'>Expense tracker</h1>
+      <div className="bg-neutral-950 text-white h-screen flex justify-center items-center">
+        <div className="w-2/5 bg-neutral-800 p-10 rounded-md w-full">
+          <header className="text-4xl font-bold text-center my-5">
+            Expense Tracker
+          </header>
+          <main className="flex flex-col md:flex-row">
+            <section>
+              Formulario de ingresos/egresos
+            </section>
+            <section>
+              Estadisticas y la lista de ingresos/egresos
+            </section>
+          </main>
+        </div>
+      </div>
     </>
   )
 }
